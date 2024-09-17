d90f68c4693f7cf810b64625513dcb363720c1e7
Garu006 <garojas@uamv.edu.ni>
2024-09-17 06:33:11 -0600

    Ejercicio 1

---
+++ Ejercicio1/Ejercicio1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using Ejercicio1.models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class Form1 : Form
+    {
+        Dados dadillo = new Dados();
+        private const int lanzamientos = 3600;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lanzar();
+        }
+
+        private void lanzar()
+        {
+           dadillo.LanzarDado(lanzamientos);
+            int[] resultado = dadillo.Resultado();
+            lbResultado.Items.Clear();
+
+            for (int i = 0; i <= 12; i++)
+            {
+                lbResultado.Items.Add($"Suma: {i}" + $"---------- Frecuencia: {resultado[i]}");
+            }
+
+            MessageBox.Show($"El numero 7 aparecio {resultado[7]} veces", "resultados", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ Ejercicio1/Ejercicio1/models/Dados.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.models
+{
+    internal class Dados
+    {
+        private int[] Dado = new int[13];
+        public void LanzarDado(int intento)
+        {
+            Random rand = new Random();
+
+            Random aleatorio = new Random();
+
+            for(int i=0; i<intento; i++)
+            {
+                int dado1=rand.Next(1,7);
+
+                int dado2 = rand.Next(1, 7);
+
+                int suma = dado1 + dado2;
+
+                
+            }
+        }
+
+        public int[] Resultado()
+        {
+            return Dado;
+        }
+    }
+}
